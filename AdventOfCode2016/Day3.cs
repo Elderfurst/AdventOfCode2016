663903a0b1d4014ae9a3270c734eccf6ac6bf9b6
Nick Anderson <nanderson@reliaslearning.com>
2017-10-25 14:25:59 -0400

    Day 3 Solution

---
+++ AdventOfCode2016/Day3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016
+{
+    public class Day3 : IRunnable
+    {
+        public void Run()
+        {
+            var input = File.ReadAllText(@"C:\Temp\Day3Input.txt");
+            var triangles = input.Split(new string[] {"\r\n"}, StringSplitOptions.None)
+                .Select(x => x.Trim().Split().Where(y => !string.IsNullOrEmpty(y))).ToArray();
+            var possible = 0;
+
+            for (var i = 0; i < triangles[0].Count(); i++)
+            {
+                for (var j = 0; j < triangles.Length - 2; j = j + 3)
+                {
+                    if (int.Parse(triangles[j].ElementAt(i)) + int.Parse(triangles[j + 1].ElementAt(i)) > int.Parse(triangles[j + 2].ElementAt(i)) && int.Parse(triangles[j + 1].ElementAt(i)) + int.Parse(triangles[j + 2].ElementAt(i)) > int.Parse(triangles[j].ElementAt(i)) && int.Parse(triangles[j].ElementAt(i)) + int.Parse(triangles[j + 2].ElementAt(i)) > int.Parse(triangles[j + 1].ElementAt(i)))
+                    {
+                        possible++;
+                    }
+                }
+            }
+
+/*            foreach (var currentTriangle in input.Split(new string[] {"\r\n"}, StringSplitOptions.None))
+            {
+                var triangle = currentTriangle.Trim().Split();
+                triangle = triangle.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                if (int.Parse(triangle[0]) + int.Parse(triangle[1]) > int.Parse(triangle[2]) && int.Parse(triangle[1]) + int.Parse(triangle[2]) > int.Parse(triangle[0]) && int.Parse(triangle[0]) + int.Parse(triangle[2]) > int.Parse(triangle[1]))
+                {
+                    possible++;
+                }
+            }*/
+            Console.WriteLine(possible);
+        }
+    }
+}
--- AdventOfCode2016/Program.cs
+++ AdventOfCode2016/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2016
     {
         public static void Main(string[] args)
         {
-            var run = new Day2();
+            var run = new Day3();
             run.Run();
         }
     }
