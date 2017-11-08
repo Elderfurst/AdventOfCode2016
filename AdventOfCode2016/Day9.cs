c77b2bbd676096e16fbbf1b0d170c4567b3faea7
Nick Anderson <nanderson@reliaslearning.com>
2017-11-08 15:56:07 -0500

    Day 9 starting files

---
+++ AdventOfCode2016/Day9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016
+{
+    public class Day9 : IRunnable
+    {
+        public void Run()
+        {
+            PartOne();
+            PartTwo();
+        }
+
+        private void PartOne()
+        {
+            
+        }
+
+        private void PartTwo()
+        {
+            
+        }
+    }
+}
